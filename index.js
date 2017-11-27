27a9defd88bc39d2200b3f4ea1ace20d25f1e597
Ryan Inawat <rrinawat@gmail.com>
2017-11-27 19:45:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+//addition
+function add(a,b){
+  return a+b
+}
+//subtraction
+function subtract(a,b){
+  return a-b
+}
+//multiply
+function multiply(a,b){
+  return a * b
+}
+//divide
+function divide(a,b){
+  return a / b
+}
+//increment
+function inc(n){
+  return n++
+}
